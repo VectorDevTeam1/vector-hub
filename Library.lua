7afcb48faa7603c96962284c1e6da90d997131c1
VectorDevTeam1 <gtaflow228@gmail.com>
2026-09-24 04:54:33 +0300

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -3024,7 +3024,7 @@ end
 
 Window:Line()
 
-local RecorderInit = loadstring(game:HttpGet("https://raw.githubusercontent.com/DuxiiT/auto-strat/refs/heads/main/Sources/Recorder.lua"))()
+local RecorderInit = loadstring(game:HttpGet("https://raw.githubusercontent.com/VectorDevTeam1/vector-hub/refs/heads/main/Sources/Recorder.lua"))()
 RecorderInit({
     Window = Window,
     ReplicatedStorage = ReplicatedStorage,
